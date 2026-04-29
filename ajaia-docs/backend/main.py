3df11dad1f4404db8a02484e8dc4e5d3c74cd202
LasyaRamachandruni <lasyaramachandruni@gmail.com>
2026-04-29 13:22:17 -0700

    add vercel urls to cors

--- ajaia-docs/backend/main.py
+++ ajaia-docs/backend/main.py
@@ -12,7 +12,7 @@ app = FastAPI(title="Ajaia Docs API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "https://lasya-five.vercel.app", "https://lasya-3fhkdgivu-lasyaramachandrunis-projects.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
